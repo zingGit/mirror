08f16410fef142d4ccfce28f57aa14ede6cdb1f2
zingYnu <zing@tndstd.com>
2021-11-05 12:05:53 +0800

    1

---
+++ src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/first.fire",orientation:"landscape",jsList:["assets/Script/utils/crypto-js.js"]};
